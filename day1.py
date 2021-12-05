f86ac8709382d6ad88802be154ae41fab584da7f
Jurij Waite <jurijlwaite@gmail.com>
2021-12-05 03:32:33 -0800

    Implemented a (basic) solution to part 2 of day 1.

--- day1.py
+++ day1.py
@@ -5,6 +5,8 @@ def main():
     with open("Inputs/input1.txt", "r") as f:
         data = [int(depth.strip()) for depth in f.readlines()]
 
+        # PART A
+
         def differences(lst, count=0):
             """
             Calculate the number of sequential increases in a list of numbers.
@@ -68,6 +70,35 @@ def main():
         print("Split Differences")
         print(f"Number of depth increases: {split_differences(data)}")
         print("No additional recursion depth required!")
+
+        # PART B
+        # Despite the problem asking for us to compare sums of sets of 3 consecutive depths,
+        # because any two consecutive sets share two numbers in common, we can just compare
+        # the 1st to 4th depth, the 2nd to 5th depth, and so on, without having to compare
+        # the elements in between.
+
+        def group_differences(lst, count=0):
+            """
+            Calculate the number of increases in consecutive sums of 3 numbers.
+            Uses the fact that doing so corresponds to comparing every element, to
+            the number 3 indices higher.
+            :param lst: List of numbers, which are checked for differences.
+            :param count: Internal parameter that tracks the number of differences.
+            :return: The number of increases in consecutive sums of 3 numbers.
+            """
+            if len(lst) <= 3:
+                return count
+            else:
+                if lst[-4] < lst[-1]:
+                    count += 1
+                return group_differences(lst[:-1], count)
+
+        with recursionlimit(len(data) + 1):
+            print("----Group Differences Method----")
+            print(f"Number of increases in consecutive sums of 3 depths: {group_differences(data)}")
+            print(f"Requires a recursion depth of: {len(data) + 1}")
+
+
 
 
 if __name__ == "__main__":
